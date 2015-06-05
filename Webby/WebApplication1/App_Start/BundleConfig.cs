9aab282150710265f0ec9c951f1c9cf9b9f30ab4
Maximilianm <maximilianm@applicon.se>
2015-06-05 10:48:12 +0200

    new scroll fix some html 5 things some script mgnt

--- Webby/WebApplication1/App_Start/BundleConfig.cs
+++ Webby/WebApplication1/App_Start/BundleConfig.cs
@@ -8,33 +8,32 @@ namespace WebApplication1
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js",
-                        "~/Scripts/jquery.scrollTo.min.js"
+                        "~/Scripts/jquery-{version}.min.js"
                         ));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.unobtrusive*",
                         "~/Scripts/jquery.validate*"
                         ));
 
             bundles.Add(new ScriptBundle("~/bundles/js").Include(
-                      "~/Scripts/bootstrap.min.js",
-                      "~/Scripts/respond.min.js",
-                      "~/Scripts/underscore.js",
-                      "~/Scripts/jquery.signalR-2.2.0.min.js"
-                      ));
+                        "~/Scripts/bootstrap.min.js",
+                        "~/Scripts/jquery.signalR-2.2.0.min.js",
+                        "~/Scripts/respond.min.js",
+                        "~/Scripts/underscore.js",
+                        "~/Scripts/jquery-scrollto.js"
+                        ));
+
+            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"
+                        ));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css",
                       "~/Content/clean-blog.css"
                       ));
-
-
-
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"
-                        ));
 
             //!!Comment out if you don't want minification!!.
             BundleTable.EnableOptimizations = true;

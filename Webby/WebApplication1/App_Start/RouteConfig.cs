5b8661008873e7a91bd1157abdedc788f638838b
Maximilianm <maximilianm@applicon.se>
2015-12-01 14:35:03 +0100

    fixed scrolllist

--- Webby/WebApplication1/App_Start/RouteConfig.cs
+++ Webby/WebApplication1/App_Start/RouteConfig.cs
@@ -12,6 +12,18 @@ namespace WebApplication1
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.IgnoreRoute("{resource}.ashx/{*pathInfo}");
+            routes.IgnoreRoute("{*allashx}", new { allashx = @".*\.ashx" });
+            routes.IgnoreRoute("{*allaspx}", new { allaspx = @".*\.aspx" });
+            routes.IgnoreRoute("{*alljs}", new { alljs = @".*\.js" });
+            routes.IgnoreRoute("{*allpng}", new { allpng = @".*\.png" });
+            routes.IgnoreRoute("{*alljpg}", new { alljpg = @".*\.jpg" });
+            routes.IgnoreRoute("{*allgif}", new { allgif = @".*\.gif" });
+            routes.IgnoreRoute("{*allcss}", new { allcss = @".*\.css" });
+            routes.IgnoreRoute("{*allwoff}", new { allcss = @".*\.woff" });
+            routes.IgnoreRoute("{*alleot}", new { allcss = @".*\.eot" });
+            routes.IgnoreRoute("{*allsvg}", new { allcss = @".*\.svg" });
+            routes.IgnoreRoute("{*alltff}", new { allcss = @".*\.tff" });
 
             routes.MapRoute(
                 name: "Default",
